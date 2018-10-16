005dd1a08a8a1ef998672fb036cdaf57a9cd41ee
bjxuboru <bjxuboru@corp.netease.com>
2018-10-16 18:25:52 +0800

    3数字之和

--- src/main/java/com/troy/algorithm/ThreeSum.java
+++ src/main/java/com/troy/algorithm/ThreeSum.java
@@ -1,6 +1,11 @@
 package com.troy.algorithm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * @author: xuboru on 2018/10/12
@@ -19,8 +24,48 @@ import java.util.List;
  */
 public class ThreeSum
 {
-	public List<List<Integer>> threeSum(int[] nums)
+	public List<List<Integer>> threeSum(int[] nums) {
+		List<List<Integer>> res = new ArrayList();
+		Arrays.sort(nums);
+		for(int k=0;k<nums.length;k++){
+			int i = k+1,j = nums.length - 1;//***
+			while(i < j){
+				int value = nums[i] + nums[j];
+				if(value == (-nums[k])){
+					List<Integer> list = new ArrayList();
+					list.add(nums[k]);
+					list.add(nums[i]);
+					list.add(nums[j]);
+					res.add(list);
+					//重值处理
+					//****一定要有i < j条件，否则i会到最后
+					while(i<j && nums[i] == nums[i+1]){
+						i++;
+					}
+					//****一定要有i < j条件，否则j会取-1
+					while(i<j && nums[j] == nums[j-1]){
+						j--;
+					}
+					//***
+					i++;
+					j--;
+				}
+				else if(value < (-nums[k]))
+					i++;
+				else
+					j--;
+			}
+			//****重值处理
+			while(k < nums.length-1 && nums[k] == nums[k+1]){
+				k++;
+			}
+		}
+		return res;
+	}
+
+
+	public static void main(String[] args)
 	{
-		return null;
+		System.out.println(new ThreeSum().threeSum(new int[] { -1, 0, 1, 2, -1, -4 }));
 	}
 }
