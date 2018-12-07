1d3f4d9d0e2152ace69ff02a2d127ef0a8a5145f
xuboru <xuboru@corp.rrzcp8.com>
2018-12-07 11:55:01 +0800

    简单的java自旋锁

---
+++ src/main/java/com/troy/lock/SpinLock.java
@@ -0,0 +1,27 @@
+package com.troy.lock;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+/**
+ * @author: xuboru on 2018/12/7
+ * 简单的java自旋锁
+ */
+public class SpinLock
+{
+	private AtomicReference<Thread> cas = new AtomicReference<>();
+
+	public void lock()
+	{
+		Thread thread = Thread.currentThread();
+		while (!cas.compareAndSet(null, thread))
+		{
+			//do nothing
+		}
+	}
+
+	public void unlock()
+	{
+		Thread thread = Thread.currentThread();
+		cas.compareAndSet(thread, null);
+	}
+}
