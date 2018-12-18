1f04b61bcc4d134da5f22339bdd39e2261dd0056
xuboru <xuboru@corp.rrzcp8.com>
2018-12-18 16:47:06 +0800

    回文链表

--- src/main/java/com/troy/algorithm/IsPalindrome.java
+++ src/main/java/com/troy/algorithm/IsPalindrome.java
@@ -20,6 +20,50 @@ public class IsPalindrome
 		return true;
 	}
 
+	/**
+	 * 234. 回文链表
+	 * https://leetcode-cn.com/problems/palindrome-linked-list/
+	 * @param head
+	 * @return
+	 */
+	public boolean isPalindrome(ListNode head)
+	{
+		if (head == null)
+			return true;
+		//使用快慢指针找到中间节点，反转后面的链表，然后双链表进行比较
+		ListNode slow, fast, mid;
+		for (slow = head, fast = head != null ? head.next : null;
+			 fast != null; slow = slow.next, fast = fast.next.next)
+		{
+			if (fast.next == null)
+			{
+				break;
+			}
+		}
+		mid = slow.next;
+		slow.next = null;
+		mid = reverseList(mid);
+		for (; head != null && mid != null; head = head.next, mid = mid.next)
+		{
+			if (head.val != mid.val)
+				return false;
+		}
+		return true;
+	}
+
+	private ListNode reverseList(ListNode head)
+	{
+		ListNode pre, current;
+		for (pre = null, current = head; current != null; )
+		{
+			ListNode next = current.next;
+			current.next = pre;
+			pre = current;
+			current = next;
+		}
+		return pre;
+	}
+
 	public static void main(String[] args)
 	{
 		System.out.println(new IsPalindrome().isPalindrome(10));
