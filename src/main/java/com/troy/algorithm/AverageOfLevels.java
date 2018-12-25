8314c6add0908ccdae97ccd2b7f23ce287acc99b
xuboru <xuboru@corp.rrzcp8.com>
2018-12-25 16:35:14 +0800

    二叉树的层平均值

--- src/main/java/com/troy/algorithm/AverageOfLevels.java
+++ src/main/java/com/troy/algorithm/AverageOfLevels.java
@@ -7,6 +7,9 @@ import java.util.Queue;
 
 /**
  * @author: xuboru on 2018/12/25
+ * 637. 二叉树的层平均值
+ * https://leetcode-cn.com/problems/average-of-levels-in-binary-tree/
+ *
  */
 public class AverageOfLevels
 {
