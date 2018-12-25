e301644020710c97ebcc36f978c7ee0e54cd42b3
xuboru <xuboru@corp.rrzcp8.com>
2018-12-25 14:46:48 +0800

    N叉树的最大深度

--- src/main/java/com/troy/algorithm/MaxDepth.java
+++ src/main/java/com/troy/algorithm/MaxDepth.java
@@ -13,4 +13,27 @@ public class MaxDepth
 	{
 		return root == null ? 0 : 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 	}
+
+	/**
+	 * 559. N叉树的最大深度
+	 * https://leetcode-cn.com/problems/maximum-depth-of-n-ary-tree/
+	 * @param root
+	 * @return
+	 */
+	public int maxDepth(Node root)
+	{
+		if (root == null)
+			return 0;
+		int max = 0;
+		if (root.children != null)
+		{
+			for (Node node : root.children)
+			{
+				int x = maxDepth(node);
+				if (x > max)
+					max = x;
+			}
+		}
+		return max + 1;
+	}
 }
