1044101283c6f37706bfe28599a296d819d86b96
xuboru <xuboru@corp.rrzcp8.com>
2018-12-25 16:08:52 +0800

    N叉树的层序遍历

--- src/main/java/com/troy/algorithm/LevelOrder.java
+++ src/main/java/com/troy/algorithm/LevelOrder.java
@@ -52,4 +52,33 @@ public class LevelOrder
 		}
 		return list;
 	}
+
+	/**
+	 * 429. N叉树的层序遍历
+	 * https://leetcode-cn.com/problems/n-ary-tree-level-order-traversal/
+	 * @param root
+	 * @return
+	 */
+	public List<List<Integer>> levelOrder(Node root)
+	{
+		List<List<Integer>> result = new ArrayList<>();
+		if (root == null)
+			return result;
+		Queue<Node> queue=new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()){
+			List<Integer> list=new ArrayList<>();
+			Queue<Node> nextQueue=new LinkedList<>();
+			while (!queue.isEmpty()){
+				Node node =queue.poll();
+				list.add(node.val);
+				if (node.children!=null){
+					node.children.forEach(nextQueue::add);
+				}
+			}
+			result.add(list);
+			queue= nextQueue;
+		}
+		return result;
+	}
 }
